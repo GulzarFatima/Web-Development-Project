8a88ff31421c16b29e052f07f382ba1d804d685d
Gulzar Fatima <gul.aka.alukebab@gmail.com>
2025-06-19 14:01:06 -0400

    Added Week 7 Lab

---
+++ week7/delete.php
@@ -0,0 +1,11 @@
+<?php
+  require('connect.php');
+
+    $id = $_GET['id'];
+    $query = "DELETE FROM schools WHERE id = '$id'";
+    $result = mysqli_query($connect, $query);
+    if($result){
+        header('Location: index.php');
+
+    }
+    
